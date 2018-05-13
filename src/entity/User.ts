245fde1cfb2d1419e140d767f08d60f4296b1730
rubenbase <daggosso@hotmail.com>
2018-05-14 00:57:50 +0200

    Added @types + uuid control for ids creation

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,22 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, PrimaryColumn, BeforeInsert} from "typeorm";
+import uuidv4 = require('uuid/v4');
+
 
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+    @PrimaryColumn("uuid") 
+    id: string;
 
-    @Column()
-    firstName: string;
+    @Column("varchar", {length: 255})
+    email: string;
 
-    @Column()
-    lastName: string;
+    @Column("text")
+    password: string;
 
-    @Column()
-    age: number;
+    @BeforeInsert()
+    addId(){
+        this.id = uuidv4();
+    }
 
 }
