1e531e2055792693b4b1b886ad643375c1a7c027
rubenbase <daggosso@hotmail.com>
2018-05-17 20:03:58 +0200

    Shared schema to handle erros + Improve id generation + test errors + force exit after test

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,23 +1,11 @@
-import {
-  Entity,
-  Column,
-  PrimaryColumn,
-  BeforeInsert,
-  BaseEntity
-} from "typeorm";
-import uuidv4 = require("uuid/v4");
+import { Entity, Column, BaseEntity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("users")
 export class User extends BaseEntity {
-  @PrimaryColumn("uuid") id: string;
+  @PrimaryGeneratedColumn("uuid") id: string;
 
   @Column("varchar", { length: 255 })
   email: string;
 
   @Column("text") password: string;
-
-  @BeforeInsert()
-  addId() {
-    this.id = uuidv4();
-  }
 }
